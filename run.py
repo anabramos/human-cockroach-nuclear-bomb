7296ecccba8e7c4a31897f91e1b22983c598741c
anabramos <anaramosbarretto@gmail.com>
2021-10-13 18:12:10 +0000

    Import built in libraries and get player details

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import math
+import random
+
+def get_player_data():
+    """
+    Get player details
+    """
+    print("Please enter your battle name")
+    print("Battle names can have a maximu of 20 characters,")
+    print("and spaces must be separated by an underscore")
+    print("(for example: ana_banana) \n")
+
+    battle_name = input("Enter your battle name here:")
+    print(f"Welcome to the battle, {battle_name}")
+
+
+print("Welcome to Human, Cockroach and Nuclear Bomb! \n")
+get_player_data()
