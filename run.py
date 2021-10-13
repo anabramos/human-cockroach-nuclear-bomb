7e803dc92f1810ce20c5bd936416c0078d47d606
anabramos <anaramosbarretto@gmail.com>
2021-10-13 18:48:59 +0000

    Validate player's battle name to be shorter than 20 characters and contain no spaces

--- run.py
+++ run.py
@@ -11,7 +11,26 @@ def get_player_data():
     print("(for example: ana_banana) \n")
 
     battle_name = input("Enter your battle name here:")
-    print(f"Welcome to the battle, {battle_name}")
+    validate_battle_name(battle_name)
+
+    #print(f"Welcome to the battle, {battle_name}")
+
+def validate_battle_name(battle_name):
+    """
+    Raise an Error in case battle name has more than 20 characters, 
+    and/or has a space character. 
+    """
+    try:
+        if len(battle_name) > 20:
+            raise ValueError(
+                f"Battle names can mave a maximum of 20 characters. Yours has {len(battle_name)}"
+            )
+        elif ' ' in battle_name:
+            raise ValueError(
+                f"You must use underscore to represent spaces"
+            )
+    except ValueError as e:
+        print(f"Error: {e}. Please try again")
 
 
 print("Welcome to Human, Cockroach and Nuclear Bomb! \n")
